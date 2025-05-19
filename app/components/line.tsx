155f42487be4918795dc0de0ed3eb0b5685c3ead
FruitTo <krissada.22447@gmail.com>
2025-05-19 18:37:18 +0700

    fix bug line

--- app/components/line.tsx
+++ app/components/line.tsx
@@ -7,8 +7,34 @@ const Line = ({ line }:{line:string[]}) => {
   let rotate = "0";
   [h, w, top, left, rotate] = line;
 
+    // <div className="absolute top-2/12 w-full h-1 bg-yellow-400 z-50"></div>
+    // <div className="absolute top-6/12 w-full h-1 bg-yellow-400 z-50"></div>
+    // <div className="absolute top-10/12 w-full h-1 bg-yellow-400 z-50"></div>
+
+    // <div className="absolute left-2/12 w-1 h-full bg-yellow-400 z-50"></div>
+    // <div className="absolute left-6/12 w-1 h-full bg-yellow-400 z-50"></div>
+    // <div className="absolute left-10/12 w-1 h-full bg-yellow-400 z-50"></div>
+
+    // <div className="absolute left-[-70px] top-[190px] rotate-45 w-[525px] h-1 bg-yellow-400 z-50"></div>
+    // <div className="absolute left-[-70px] top-[190px] rotate-135 w-[525px] h-1 bg-yellow-400 z-50"></div>
+
+
+
+
   return (
-    <div className={`absolute bg-yellow-400 z-50 h-${h} w-${w} top-${top} left-${left} rotate-${rotate}`}></div>
+    <>
+     {/* <div className="absolute top-2/12 w-full h-1 bg-yellow-400 z-50"></div>
+     <div className="absolute top-6/12 w-full h-1 bg-yellow-400 z-50"></div>
+     <div className="absolute top-10/12 w-full h-1 bg-yellow-400 z-50"></div>
+
+     <div className="absolute left-2/12 w-1 h-full bg-yellow-400 z-50"></div>
+     <div className="absolute left-6/12 w-1 h-full bg-yellow-400 z-50"></div>
+     <div className="absolute left-10/12 w-1 h-full bg-yellow-400 z-50"></div>
+
+     <div className="absolute left-[-70px] top-[190px] rotate-45 w-[525px] h-1 bg-yellow-400 z-50"></div>
+     <div className="absolute left-[-70px] top-[190px] rotate-135 w-[525px] h-1 bg-yellow-400 z-50"></div> */}
+      <div className={`absolute bg-yellow-400 z-50 h-${h} w-${w} top-${top} left-${left} rotate-${rotate}`}></div>
+    </>
   );
 };
 export default Line;
