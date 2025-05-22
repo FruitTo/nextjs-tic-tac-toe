66679d28f2bf1b9f0b38c8d1baa7ee1deaca499e
FruitTo <krissada.22447@gmail.com>
2025-05-22 11:15:33 +0700

    comment next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/tic-tac-toe',
-  trailingSlash: true
+  // output: 'export',
+  // basePath: '/tic-tac-toe',
+  // trailingSlash: true
 };
 
 
