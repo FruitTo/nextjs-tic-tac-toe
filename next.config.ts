f61194131b9e90b28bf4fb9542eafe8e4aef46af
FruitTo <krissada.22447@gmail.com>
2025-05-22 11:11:20 +0700

    delete build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/tic-tac-toe',
+  trailingSlash: true
 };
+
 
 export default nextConfig;
